2ba75ea6945e1a8f37e28b8c9405e25dc8b034ad
valentin-pronkin <92966641+valentin-pronkin@users.noreply.github.com>
2024-10-10 15:04:31 +0400

    Config sample added

---
+++ config.sample.php
@@ -0,0 +1,17 @@
+<?php
+
+const TG_TOKEN = '5396852110:AAF8bxI9EkLPpfsRM7WgLfCszIRvGM6rJQE';
+const TG_ADMIN_ID = [1116558542, 1116558543];
+
+const DB_HOST = "localhost";
+const DB_DB = "dbname";
+const DB_USER = "user";
+const DB_PASS = "pass";
+
+const TP_DELIVERY_1_TOD = [12, 30];     // Time of day of first delivery [hours, minutes]
+const TP_DELIVERY_2_TOD = [16, 30];     // Time of day of second delivery [hours, minutes]
+const TP_FULL_PROCESS_TIME = [4, 0];    // Full process time from billet to ready-to-sell product [hours, minutes]
+
+date_default_timezone_set('America/Asuncion');
+
+?>
